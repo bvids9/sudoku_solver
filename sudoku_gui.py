e6c23ecf8c5d6bdf7d619977aab96c297cd6a09c
Brent Vidler <you@example.com>
2020-08-04 09:51:54 +0930

    feature complete. solves and generates

--- sudoku_gui.py
+++ sudoku_gui.py
@@ -12,14 +12,16 @@ class SudokuUI(Frame):
 
         # Load an initial board
         self.sudoku = SudokuBoard()
-        self.empty_board = self.sudoku.gen_board()
 
         # UI Code
         Frame.__init__(self, parent)
         self.parent = parent
         self.disp_row, self.disp_col = -1, -1
 
+        self.board_loaded = False
+
         self.__generate_interface()
+
         pass
 
     def __generate_interface(self):
@@ -39,11 +41,20 @@ class SudokuUI(Frame):
 
         draw_button.pack(fill=BOTH, side=BOTTOM)
 
+        solve_button = Button(self,
+                            text="Solve Puzzle",
+                            command=self.draw_answers_command)
+        solve_button.pack(fill=BOTH, side=BOTTOM)
+
         # TODO: Add Clear Answers Button
         # TODO: Add Solve Button
 
         self.__draw_grid()
+        self.puzzle_board, self.solution = self.__get_board()
 
+    def __get_board(self):
+        puzzle_board, solution = self.sudoku.gen_puzzle_board(level="medium")
+        return puzzle_board, solution
 
     def __draw_grid(self):
         # Draw the initial grid
@@ -67,33 +78,44 @@ class SudokuUI(Frame):
 
             self.canvas.create_line(x0, y0, x1, y1, fill=colour)
 
+    def __get_num_coords(self, i, j):
+        # Get coordinates for text generation
+        x = MARGIN + j * SIDE + SIDE/2
+        y = MARGIN + i * SIDE + SIDE/2
+
+        return (x,y)
 
     def __draw_numbers(self, difficulty, original=False, answers=False, visual_solve=False):
         # Load numbers into the board
         self.canvas.delete("numbers")
+        self.canvas.delete("solution")
         # TODO: Change difficulty levels
-        # Consider refactoring the board_generation into a seperate function.
-        puzzle_board, solution = self.sudoku.gen_puzzle_board(level=difficulty)
 
-        if original:    # Load in original numbers
+        # Loads new board
+        self.puzzle_board, self.solution = self.__get_board()
+
+            # Load in original numbers
+            # Consider refactoring the below...
+        for i in range(9):
+            for j in range(9):
+                answer = self.puzzle_board[i][j]
+                if answer != 0:
+                    x, y = self.__get_num_coords(i,j)
+                    self.canvas.create_text(
+                        x,y, text=answer, tags="numbers", fill="black"
+                    )
+                    self.update()   #Tkinter.update function to refresh screen
+                    time.sleep(delay)
+        self.board_loaded = True
+
+        # Consider seperating out for a solution drawing
+    def __draw_solution(self):
+        if self.board_loaded:
             for i in range(9):
                 for j in range(9):
-                    answer = puzzle_board[i][j]
-                    if answer != 0:
-                        x = MARGIN + j * SIDE + SIDE/2
-                        y = MARGIN + i * SIDE + SIDE/2
-                        self.canvas.create_text(
-                            x,y, text=answer, tags="numbers", fill="black"
-                        )
-                        self.update()   #Tkinter.update function to refresh screen
-                        time.sleep(delay)
-        if answers:
-            for i in range(9):
-                for j in range(9):
-                    answer = solution[i][j]
-                    if puzzle_board[i][j] == 0:
-                        x = MARGIN + j * SIDE + SIDE/2
-                        y = MARGIN + i * SIDE + SIDE/2
+                    answer = self.solution[i][j]
+                    if self.puzzle_board[i][j] == 0:
+                        x, y = self.__get_num_coords(i,j)
                         self.canvas.create_text(
                             x,y, text=answer, tags="solution", fill="sea green"
                         )
@@ -101,8 +123,8 @@ class SudokuUI(Frame):
                         time.sleep(delay)
 
         
-        # Code to generate the iteration and solving.
-        # Will need to generate another internal function
+        # TODO: Code to generate the iteration and solving.
+        #       Will need to generate another internal function to do the backtracking.
     
     def draw_command(self):
         # Call the draw_numbers function for the button
@@ -110,7 +132,7 @@ class SudokuUI(Frame):
     
     def draw_answers_command(self):
         # Call the draw_numbers function for answer button
-        self.__draw_numbers(difficulty="medium", original=False, answers=True)
+        self.__draw_solution()
 
     def visual_solver(self, board):
         # Function for the visual solving
@@ -124,5 +146,5 @@ class SudokuUI(Frame):
 
 root = Tk()
 SudokuGame = SudokuUI(root)
-root.geometry(f"{WIDTH}x{HEIGHT+40}")
+root.geometry(f"{WIDTH}x{HEIGHT+120}")
 root.mainloop()
